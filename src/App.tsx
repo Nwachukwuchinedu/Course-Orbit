f0c5625b1f7ee3383f4701b7a91667c6311a778e
Nwachukwuchinedu <chinedu.simeon2020@gmail.com>
2025-02-11 14:00:55 +0100

    installed vercel abalytics

--- src/App.tsx
+++ src/App.tsx
@@ -11,60 +11,64 @@ import Admin from "./pages/Admin";
 import NotFound from "./pages/NotFound";
 import { AuthProvider } from "./components/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <div className="flex flex-col min-h-screen">
-          <Navbar />
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<AuthForm type="login" />} />
-              <Route path="/signup" element={<AuthForm type="signup" />} />
-              <Route
-                path="/dashboard"
-                element={
-                  // <ProtectedRoute>
-                  //   <Dashboard />
-                  // </ProtectedRoute>
-                  <Dashboard />
-                }
-              />
-              <Route
-                path="/course/:courseId"
-                element={
-                  // <ProtectedRoute>
-                  //   <CourseDetails />
-                  // </ProtectedRoute>
-                  <CourseDetails />
-                }
-              />
-              <Route
-                path="/pricing"
-                element={
-                  <ProtectedRoute>
-                    <Pricing />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/admin"
-                element={
-                  <ProtectedRoute>
-                    <Admin />
-                  </ProtectedRoute>
-                }
-              />
-              {/* Add the 404 route */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
-    </AuthProvider>
+    <>
+      <AuthProvider>
+        <Router>
+          <div className="flex flex-col min-h-screen">
+            <Navbar />
+            <main className="flex-grow">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<AuthForm type="login" />} />
+                <Route path="/signup" element={<AuthForm type="signup" />} />
+                <Route
+                  path="/dashboard"
+                  element={
+                    // <ProtectedRoute>
+                    //   <Dashboard />
+                    // </ProtectedRoute>
+                    <Dashboard />
+                  }
+                />
+                <Route
+                  path="/course/:courseId"
+                  element={
+                    // <ProtectedRoute>
+                    //   <CourseDetails />
+                    // </ProtectedRoute>
+                    <CourseDetails />
+                  }
+                />
+                <Route
+                  path="/pricing"
+                  element={
+                    <ProtectedRoute>
+                      <Pricing />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/admin"
+                  element={
+                    <ProtectedRoute>
+                      <Admin />
+                    </ProtectedRoute>
+                  }
+                />
+                {/* Add the 404 route */}
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </AuthProvider>
+      <Analytics />
+    </>
   );
 }
 
